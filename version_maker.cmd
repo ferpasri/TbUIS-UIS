ac315a1e2e71663a7bb7d324a8d38f90ac45a349
Radek Vais <vaisr@students.zcu.cz>
2019-09-24 23:12:32 +0200

    Přidání scriptu pro dávkové vygenerování verzí dle seed souborů ve složce version_seeds.

---
+++ version_maker.cmd
@@ -0,0 +1,36 @@
+:: Script for automatic creation of error versions
+
+::Configuration ========================================
+
+::Target directory for generated files
+set outDir=.\release_archive\generated_versions
+::Nave of directory with example seeds.
+set seedDir=.\version_seeds
+::Name of output filename of maven compilation process
+set mavenOutFileName=UIS-1.6.0.war
+
+:: =====================================================
+:: Generating versions
+
+::Backup current seed.xml
+del .\src\main\resources\seed.bck
+move .\src\main\resources\seed.xml .\src\main\resources\seed.bck
+
+::Create output directory for error versions
+mkdir %outDir%
+
+::Creating loop
+for /f %%f in ('dir /b %seedDir%') do (
+ echo %%f
+ copy .\%seedDir%\%%f .\src\main\resources\seed.xml
+ mvn clean
+ mvn package
+ move /Y .\target\%mavenOutFileName% %outDir%\%%~nf.war
+)
+
+::Clean after building
+mvn clean
+
+::Return backup file
+del .\src\main\resources\seed.xml
+move .\src\main\resources\seed.bck .\src\main\resources\seed.xml
